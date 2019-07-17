eaa93a4048ad144bae2ffd50475e8b9875e4452b
Akshay Kumar <aksh0001@student.monash.edu>
2019-07-17 20:11:25 +0530

    Modified soup finding to strip price information details

--- api/WatchService.py
+++ api/WatchService.py
@@ -9,6 +9,7 @@ import requests, smtplib
 from bs4 import BeautifulSoup
 from decimal import Decimal
 from email.mime.text import MIMEText
+from re import sub
 
 
 class WatchService(object):
@@ -20,8 +21,8 @@ class WatchService(object):
         soup_tmp = BeautifulSoup(self.page.content, 'html.parser')
         self.soup = BeautifulSoup(soup_tmp.prettify(), 'html.parser')
         self.title = self.soup.find(id='productTitle').get_text().strip()
-        price = self.soup.find(id='priceblock_ourprice').get_text().strip()
-        self.conv_price = Decimal(price[1:].strip())
+        price = sub(r'[^\d.]', '', (self.soup.find(id='priceblock_ourprice').get_text()))
+        self.conv_price = Decimal(price.strip())
 
     def check_price(self):
         if self.conv_price < self.watched_product.get_watched_price():
@@ -34,16 +35,18 @@ class WatchService(object):
         server.ehlo()
         server.login(self.email_creds.get_e_uname(), self.email_creds.get_e_pw())
         server.set_debuglevel(1)
-        msg = MIMEText("""Good day!\nHere is your product:""" + self.watched_product.get_prod_url())
+        body = "Good day!\nHere is your product: " + self.watched_product.get_prod_url()
+        msg = MIMEText(body)
         sender = self.email_creds.get_e_address()  # Send to self
         recipients = self.email_creds.get_e_address()
-        msg['Subject'] = "Your:", self.title, "Is now", self.watched_product.get_watched_price(), "!"
+        msg['Subject'] = "Your: " + self.title + " Is now $" + str(self.conv_price) + "!"
         msg['From'] = sender
         msg['To'] = recipients
         server.sendmail(sender, recipients.split(','), msg.as_string())
         print("Sent!")
 
     @staticmethod
-    def query_user_agent_headers() -> str:
-        header = input("Please enter your user agent header. This can be found by searching 'what's my user agent': ")
+    def query_user_agent_headers() -> dict:
+        header = {'User-Agent': input(
+            "Please enter your user agent header. This can be found by searching ""what's my user agent"": ")}
         return header
